7d3162d872f81ece501a2c96debfa457160474be
PabloSG1 <pablo.oliveira11@aluno.senai.br>
2023-10-08 02:15:17 -0300

    criação do menu

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+function menu() {
+    let opcao = Number(prompt("Olá, oque deseja executar ?\n1 - Cadastrar Despesa\n2 - Emitir Relatorio\n3 - Sair"));
+    switch (opcao) {
+        case 1:
+            let categoriaDespesa = Number(prompt("Qual catégoria é sua despesa:\n1 - Lanches\n2 - Livros\n3 - Transporte\n4 - Material Didático"));
+            cadastroDespesa(categoriaDespesa);
+            break;
+        case 2:
+            let relatorios = Number(prompt("Qual relatoiro deseja emitir:\n1 - Relatorio Unificado\n2 - Relatorio Detalhado"));
+            break;
+        case 3:
+            alert("Você Saiu");
+            break;
+        default:
+            alert("Informe uma opção válida!!");
+    }
+}
+function cadastroDespesa(categoriaDespesa) {
+    const livros = [];
+}
+menu();

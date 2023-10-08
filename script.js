ad32e790b200961b140b6c0789f9730b433bb47e
PabloSG1 <pablo.oliveira11@aluno.senai.br>
2023-10-08 15:50:37 -0300

    Função cadastro e emissão
    criação das função cadastrar despesa e emitir relatorios.

--- script.js
+++ script.js
@@ -8,15 +8,69 @@ function menu() {
             break;
         case 2:
             let relatorios = Number(prompt("Qual relatório deseja emitir?\n1 - Relatório Unificado\n2 - Relatório Detalhado"));
+            emissaoRelatorios(relatorios);
             break;
         case 3:
             alert("Programa encerrado.");
             break;
         default:
             alert("Informe uma opção válida!!");
+            menu();
     }
 }
+// Arrays das catégorias para guardar os dados.
+const lanches = [];
+const livros = [];
+const transporte = [];
+const material = [];
 function cadastroDespesa(categoriaDespesa) {
-    const livros = [];
+    // Dados que devem ser inseridos de cada despesa.
+    const descricao = String(prompt("Informe a descrição da despesa"));
+    const valor = Number(prompt("Informe o valor da despesa"));
+    const data = prompt("Informe uma data");
+    alert("Despesa cadastrada com sucesso!!");
+    // Switch para guardar os dados na categoria escolhida através do parametro da Função
+    switch (categoriaDespesa) {
+        case 1:
+            lanches.push(descricao);
+            lanches.push(valor);
+            lanches.push(data);
+            menu();
+        case 2:
+            livros.push(descricao);
+            livros.push(valor);
+            livros.push(data);
+            menu();
+        case 3:
+            transporte.push(descricao);
+            transporte.push(valor);
+            transporte.push(data);
+            menu();
+        case 4:
+            material.push(descricao);
+            material.push(valor);
+            material.push(data);
+            menu();
+        default:
+            alert("Informe um valor válido!!");
+            menu();
+    }
+}
+// Função para emissão dos relatorios
+function emissaoRelatorios(relatorios) {
+    switch (relatorios) {
+        case 1:
+            alert(`Relatorio de despesas unificado:\n
+            Categoria: Lanche | Valor Total: R$${0}\n
+
+            Categoria: Livros | Valor Total: R$${0}\n
+
+            Categoria: Transporte | Valor Total: R$${0}\n
+
+            Categoria: Material | Valor Total: R$${0}
+            `);
+            menu();
+        case 2:
+    }
 }
 menu();
